ae9199e03e68f069c199330161423b6261a40458
rodrigodelvalle <rodrigodelvalle22@hotmail.com>
2023-08-06 00:54:38 -0300

    Ultima actualización
    Verifique que el "alert" del div y del botón quedara funcionando. También modifique la función ya que había creado un "addEventListener" innecesariamente.

--- eventos.js
+++ eventos.js
@@ -1,4 +1,13 @@
-button.addEventListener("click", funcionSaludo);
-function funcionSaludo() {
-    alert("Hola!");
+function funcionSaludo(){
+    alert ("Hola!");
 }
+let btn = document.getElementById("btn");
+let miDiv = document.getElementById('miDiv');
+
+miDiv.addEventListener("click", function() {
+    alert("Hola! Soy el div");
+});
+
+btn.addEventListener("click", function(event) {
+    event.stopPropagation(); 
+});
